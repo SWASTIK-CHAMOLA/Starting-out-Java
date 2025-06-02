fb0a86f146289f8b4ecc8fca94f533634aeef1ec
SWASTIK CHAMOLA <swastik.3814031@gmail.com>
2025-06-03 00:01:43 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+
+        System.out.print("I am feeling anxious\nand the reason is ");
+        System.out.print("Regarding the internship as well as driving license test tomorrow");
+        System.out.println("This command prints the line 'println'");
+        System.out.println("that is all there is to it just anxiety and self nagging fear of god knows what");
+
+    }
+}
