df0795f8eb5045061278a0610ebbcbc39e4e0195
SWASTIK CHAMOLA <swastik.3814031@gmail.com>
2025-06-06 20:59:37 +0530

    Create variables.java

---
+++ variables.java
@@ -0,0 +1,33 @@
+public class arithmetic {
+    public static void main (String[] args){
+       /*
+        int x = 10;
+        int y = 11;
+        int z;
+
+        //z = x+y;
+        //z = x-y;
+        //z = x*y;
+        //z = x%y;
+        System.out.println(z);
+
+         */
+    // Augmented Assignment Operator
+        int x = 10;
+        int y = 3;
+        //x += y;
+        //x -= y;
+        //x *= y;
+        //x /= y;
+        System.out.println(x);
+
+    // INCREMENT AND DECREMENT Operators
+        x = x++;
+        y = y++;
+
+    // order of operations [P-E-M-D-A-S]
+        double result = 3 + 4 * (7 - 6) / 2.0;
+        System.out.println(result);
+        
+    }
+}
